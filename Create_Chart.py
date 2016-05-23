31e6081a1a44ff014452603b798cc932b5298d15
baoheng <lingbaohegn123@hotmail.com>
2016-05-23 00:33:21 -0700

    added clickable function

---
+++ Create_Chart.py
@@ -0,0 +1,51 @@
+from openpyxl import load_workbook
+import json
+
+
+def input_file():
+    f = load_workbook(filename='G:\PycharmProjects\hello_flask\static\Debt Affordability Study Data.xlsx',
+                      data_only=True)
+    ws = f["Bonds Outstanding (Grandular)"]
+    col_title = {}
+    for first_row in ws['A1':'M1']:
+        for cell in first_row:
+            col_title[cell.column] = cell.value
+
+    info_by_row = {}
+    for num, row in enumerate(ws.iter_rows(row_offset=1)):
+        row_dict = {}
+        for cell in row:
+            if cell.value is not None:
+                row_dict[col_title[cell.column]] = cell.value
+        if len(row_dict) != 0:
+            info_by_row[num] = row_dict
+    return info_by_row
+
+
+def chart_info_by_year(info):
+    info_by_year = default_sort_by_year(info)
+    data = {}
+
+    for year in info_by_year.keys():
+        dataDict = {"type": "pie",
+                    "showInLegend": True,
+                    "indexLabel": "{y}"}
+        data_point = []
+        for pledge in info_by_year[year].keys():
+            data_point.append({"y": info_by_year[year][pledge], "legendText": pledge})
+        dataDict["dataPoints"] = data_point
+        data[year] = json.dumps(dataDict)
+    return data
+
+
+def default_sort_by_year(info):
+    data = {}
+    for item in info.values():
+        if item['DatedDate'].year not in data:
+            data[item['DatedDate'].year] = {item['RevenuePledge']: item['Series Amount']}
+        else:
+            if item['RevenuePledge'] not in data[item['DatedDate'].year].keys():
+                data[item['DatedDate'].year][item['RevenuePledge']] = item['Series Amount']
+            else:
+                data[item['DatedDate'].year][item['RevenuePledge']] += item['Series Amount']
+    return data
--- Flask_CanvasJS.py
+++ Flask_CanvasJS.py
@@ -1,49 +1,41 @@
-import json
-
-from flask import Flask, render_template
-from openpyxl import load_workbook
+from flask import Flask, render_template, request, url_for, jsonify
+import Create_Chart, json
 
 app = Flask(__name__)
+
+info = Create_Chart.input_file()
+portion_data = {}
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    info = chart_info()
-    info_keys = list(info.keys())
-    return render_template("index2.html",
-                           title="Pie Chart for " + str(info_keys[len(info_keys)-1]),
-                           chart_info=chart_info())
+    print(info)
+    info_by_year = Create_Chart.chart_info_by_year(info)
+    return render_template("index.html",
+                           title="Bonds Outstanding (Grandular)",
+                           default_chart_info=info_by_year,
+                           chart_info=info)
 
 
-def input_file_by_year():
-    f = load_workbook(filename="static\Debt Affordability Study Data.xlsx", read_only=True, use_iterators=True)
-    data_by_year = {}
-    for row in f["Bond Issuance (Granular Data)"].iter_rows(row_offset=2):
-        if row[3].value not in data_by_year.keys():
-            data_by_year[row[3].value] = {row[6].value: row[5].value}
-        else:
-            if row[6].value not in data_by_year[row[3].value].keys():
-                data_by_year[row[3].value][row[6].value] = row[5].value
-            else:
-                data_by_year[row[3].value][row[6].value] += row[5].value
-    return data_by_year
+@app.route('/process_info', methods=['POST', 'GET'])
+def process_info():
+    data = request.get_json(force=True)
+    year = list(data.keys())[0]
+    global portion_data
+    portion_data = data
+    return jsonify(success=1, output=url_for("detail_info", cur_year=year, pledge=data[year]['legendText']))
 
 
-def chart_info():
-    data_by_year = input_file_by_year()
-    data = {}
-
-    for year in data_by_year.keys():
-        dataDict = {"type": "pie",
-                "showInLegend": True,
-                "indexLabel": "{y}"}
-        data_point = []
-        for key in data_by_year[year].keys():
-            data_point.append({"y": data_by_year[year][key], "legendText": key})
-        dataDict["dataPoints"] = data_point
-        data[year] = json.dumps(dataDict)
-    return data
+@app.route('/detail_info?<cur_year>&<pledge>')
+def detail_info(cur_year, pledge):
+    full_info = {}
+    for key, data in info.items():
+        if data['DatedDate'].year == int(cur_year) and data['RevenuePledge'] == pledge:
+            full_info[key] = data
+    return render_template("detail_info.html",
+                           title=pledge + " in " + cur_year,
+                           detail_info=full_info)
 
 if __name__ == '__main__':
     app.debug = True
